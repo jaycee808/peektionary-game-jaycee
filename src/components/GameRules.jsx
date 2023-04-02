bcba5b2553679b09ce95aaafccb864688c4717b9
jaycee808 <jamie.clark@live.co.uk>
2023-04-02 21:41:43 +0100

    added sizes to game rules

--- src/components/GameRules.jsx
+++ src/components/GameRules.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 function GameRules() {
     return (
+        <div>
         <div id="game-rules" className="text-lg border-2 border-solid border-zinc-900 bg-gray-300">
             <h1>How to Play</h1>
                 <p>1. Select difficulty level</p>
@@ -11,6 +12,11 @@ function GameRules() {
                     <li>Difficult: The answer is three words. The first is a color. The second and third are an animal, food, or profession.</li>
                 </ul>
         </div>
+        <div
+        id="back-box-four"
+        className="bg-blue-700 border-2 border-solid border-zinc-900"
+      ></div>
+      </div>
 );
 }
 
